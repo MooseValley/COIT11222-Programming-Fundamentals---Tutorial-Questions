97fafad1fc8200482666e1810c240c76d1fa862a
MooseValley <Moose_Software@yahoo.com.au>
2020-03-29 22:17:48 +1000

    Add question templates - ready to work through

---
+++ Week 10 - BabyNameComparison/BabyNameComparison.java
@@ -0,0 +1,6 @@
+/*
+2. Write an application that prompts the user for three first names and concatenates
+them in every possible two-name combination so that new parents can easily
+compare them to find the most pleasing baby name. Save the file as
+BabyNameComparison.java.
+*/
