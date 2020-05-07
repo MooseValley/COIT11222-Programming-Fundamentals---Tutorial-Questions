a8819c6a4d108de402fc6bcac48ec6280b9d42b5
MooseValley <Moose_Software@yahoo.com.au>
2020-05-07 13:19:47 +1000

    Add author, comment header

--- Week 09 - JPizza/JPizza.java
+++ Week 09 - JPizza/JPizza.java
@@ -1,4 +1,8 @@
 /*
+Author: Mike OMalley
+Source: JPizza.java
+
+Java - Basic Console App - Week 09
 6. Design an application for a pizzeria. The user makes pizza order choices from list
 boxes, and the application displays the price. The user can choose a pizza size of small
 ($7), medium ($9), large ($11), or extra large ($14), and one of any number of
--- Week 10 - PigLatin/PigLatin.java
+++ Week 10 - PigLatin/PigLatin.java
@@ -1,4 +1,8 @@
 /*
+Author: Mike OMalley
+Source: PigLatin.java
+
+Java - Basic Console App - Week 10
 8. Write an application that accepts a word from a user and converts it to Pig Latin. If a
 word starts with a consonant, the Pig Latin version removes all consonants from the
 beginning of the word and places them at the end, followed by ay. For example,
